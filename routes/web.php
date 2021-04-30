de6280eb9b1ecf4981c85ce979b27cba241e9b17
swapnil.aherrao <s.aherrao@wir-machen-druck.de>
2021-04-30 16:13:46 +0530

    fix

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => ['web']], function () {
 
     Route::get('/', function () {
-        return view('welcome');
+        return redirect('/emails');
     });
 
     Auth::routes();
